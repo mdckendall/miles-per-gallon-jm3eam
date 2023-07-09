850c59097cb8a89ce1af211b1b637c4680598b4e
jm3eam <104916811+jm3eam@users.noreply.github.com>
2023-07-09 21:50:20 +0000

    Third attempt to submit change.
    Created for on repo and git the step to push a commit

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+miles = float(input('How many miles will you drive: '))
+gallons = float(input('Enter the number of gallon capacity in your vehicle: '))
+MPG = (miles)/(gallons)
+print ('See the miles per gallon capacity below:')
+print ('MPG',MPG)
+                
 #your code here!
 
 #read in the miles and read in the gallons used.
